50d97cb3019236ae8c6fba9c65c9eeaf06fb4ead
amitknows <amitknowsyou@gmail.com>
2024-06-19 09:23:46 +0530

    react element , react component

--- App.js
+++ App.js
@@ -1,42 +1,23 @@
-/***********
- * Suppose you have to create the following structutre in react
- *
- * <div id= 'parent'>
- *      <div id = 'child'>
- *          <h1></h1>
- *      </div>
- * <div id = 'child2'>
- *          <h1></h1>
- *      </div>
- * </div>
- *
- *
- * ReactElement(Object)=> HTML(Browser Understands )
- */
 import React from "react";
-import ReactDOM from "react-dom/client"
+import ReactDOM from "react-dom";
 
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello i am h1 big brother of h2"
-);
 
-/*-------------------------CORE REACT--------------------------------*/
+//React Element
 
-const div = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    heading,
-    React.createElement("h2", { id: "h2" }, "child>h2"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    heading,
-    React.createElement("h2", { id: "h2" }, "child2>h2"),
-  ]),
-]);
-/*--------------------------------------------------------------------*/
-// Now the upper code looks so messy and doesnot seems to make our life easy so here comes the SAVIUOR   "JSX"
+const heading = (
+  <h1 className="head">Namste React using JSX</h1>
+)
 
-console.log(heading); // It returns object
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(div);
+
+// React functional Component
+ const HeadingComponent = ()=>{
+  return <h1>Namste React Functinoal Component 🚀</h1>;
+  
+  
+
+ }
+
+
+const root = ReactDOM.create(document.getElementById("root"));
+
+root.render(heading)
